5ad9c0686532dd6f7d72bc7bed1cfb2d340fb424
Emmanuel Mudiaga Owighoyota <emmanuelowighoyota9@gmail.com>
2026-08-03 17:22:05 +0100

    Push all import fixes + correct package.json + render.yaml

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,4 @@
-import DashboardLayout from '@/components/DashboardLayout';
+import DashboardLayout from '../../components/DashboardLayout';
 
 export default function DashboardRootLayout({ children }: { children: React.ReactNode }) {
   return <DashboardLayout>{children}</DashboardLayout>;
